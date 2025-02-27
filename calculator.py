de8e8c87a0c70cbb1cd5c999efc5ea172fef68b6
zainab-alghaithi <zainabalghaithi@gmail.com>
2025-02-27 14:22:01 +0400

    new update for calculator

--- calculator.py
+++ calculator.py
@@ -1,22 +1,17 @@
-import math
+def calculator():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operator = input("Enter operation (+, -, *, /): ")
 
-number1 = input("Enter First number:")
-Operation = input("Enter Operation like + , - , /, e, *, sqrt, mod :")
-number2 = input("Enter Second Number:")
+    if operator == '+':
+        print(num1 + num2)
+    elif operator == '-':
+        print(num1 - num2)
+    elif operator == '*':
+        print(num1 * num2)
+    elif operator == '/':
+        print("Error! Division by zero." if num2 == 0 else num1 / num2)
+    else:
+        print("Invalid operator!")
 
-if Operation == "+":
-    print(int(number1) + int(number2))
-if Operation == "-":
-    print(float(number1) - float(number2))
-if Operation == "/":
-    print(float(number1) / float(number2))
-if Operation == "e":
-    print(pow(float(number1),float(number2)))
-if Operation == "*":
-    print(float(number1)*float(number2))
-if Operation == "sqrt":
-    print( math.sqrt(float(number1)),",",math.sqrt(float(number2)))
-if Operation == "mod":
-    print(float(number1)%float(number2))
-else:
-    print("WRONG INPUT")
+calculator()

89f10ebf507652c52e2d5565d5982665f07b3d13
LeenaAlhabsi <leenahamoodalhabsi@gmail.com>
2025-02-27 14:03:04 +0400

    sum of natural numbers

--- TestPython.py
+++ TestPython.py
@@ -1,1 +1,14 @@
-print("Hallo team")
+#Sum of Natural Numbers
+print("Sum of Natural Numbers")
+num = int(input("\nEnter a number: "))
+
+if num < 0:
+    print("\nEnter a positive number")
+else:
+    sum = 0
+    while(num > 0):
+        sum += num
+        num -= 1
+    print("\nThe sum is", sum)
+
+print("\n End of Program")
